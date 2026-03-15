04deee156e5780b872217b6b6eaa805b4e1ae875
TranThiMyTram23130340 <23130340@st.hcmuaf.edu.vn>
2026-03-15 16:36:00 +0700

    uplai code user

---
+++ demo1/src/main/java/com/example/demo1/model/User.java
@@ -0,0 +1,130 @@
+package com.example.demo1.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class User {
+    @SerializedName("id")
+
+    private int userId;
+    private int role;
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private String address;
+    private String gender;
+    private Date birthday;
+    private String status;
+    private Timestamp created_at;
+    private int orderCount;
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return getUserId();
+    }
+
+    public void setId(int id) {
+        setUserId(id);
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getOrderCount() {
+        return orderCount;
+    }
+
+    public void setOrderCount(int orderCount) {
+        this.orderCount = orderCount;
+    }
+}
