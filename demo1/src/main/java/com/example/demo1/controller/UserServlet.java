97f57aa5de4e654c42bb41fd1aa10789d2e7f74a
PhanThaoTien <23130328@st.hcmuaf.edu.vn>
2026-03-22 01:06:54 +0700

    up lại code controller: đăng nhập, đăng ký...
    ChangePasswordServlet.java, ContactServlet.java, FavoriteController.java, ForgotPasswordServlet.java, LoginController.java, LoginGoogle.java, LoginGoogleHandler.java, LogoutController.java, OrderController.java, ResendOtpServlet.java, ResetPasswordServlet.java, SignupController.java, UpdateProfileServlet.java, UserServlet.java, VerifyServlet.java

---
+++ demo1/src/main/java/com/example/demo1/controller/UserServlet.java
@@ -0,0 +1,26 @@
+package com.example.demo1.controller;
+
+import com.example.demo1.model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "UserServlet", value = "/user")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        User user = (session != null) ? (User) session.getAttribute("user") : null;
+
+        if (user == null) {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+        } else {
+            request.getRequestDispatcher("/user.jsp").forward(request, response);
+        }
+    }
+}
