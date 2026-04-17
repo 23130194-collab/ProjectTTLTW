eef586fcb32310c2a3d0b53da4c0a01626f7b999
TranThiMyTram23130340 <23130340@st.hcmuaf.edu.vn>
2026-04-18 01:13:55 +0700

    ADD model cho thông báo gồm id, nội dungm thời gian

---
+++ demo1/src/main/java/com/example/demo1/model/Notification.java
@@ -0,0 +1,69 @@
+package com.example.demo1.model;
+import java.sql.Timestamp;
+
+
+public class Notification {
+
+        private int id;
+        private Integer userId;
+        private String content;
+        private String link;
+        private Timestamp createdAt;
+        private int targetRole;
+        private int isRead;
+
+        public Notification() {}
+        public Notification(Integer userId, String content, String link, int targetRole) {
+            this.userId = userId;
+            this.content = content;
+            this.link = link;
+            this.targetRole = targetRole;
+        }
+
+        public int getIsRead() { return isRead; }
+        public void setIsRead(int isRead) { this.isRead = isRead; }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Integer getUserId() {
+            return userId;
+        }
+
+        public void setUserId(Integer userId) {
+            this.userId = userId;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+
+        public Timestamp getCreatedAt() {
+            return createdAt;
+        }
+
+        public void setCreatedAt(Timestamp createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public int getTargetRole() { return targetRole; }
+        public void setTargetRole(int targetRole) { this.targetRole = targetRole; }
+
+}
