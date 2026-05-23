d2536306137c81dd1391dd0bbd73fb6c824c1fa1
TranThiMyTram23130340 <23130340@st.hcmuaf.edu.vn>
2026-05-24 03:59:53 +0700

    ADD model cart để biết giỏ hàng đó là của user nào, và thêm id cart cho các sản phẩm để biết nó nằm trong giỏ hàng nào

---
+++ demo1/src/main/java/com/example/demo1/model/Cart.java
@@ -0,0 +1,52 @@
+package com.example.demo1.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class Cart {
+    private int id;
+    private int userId;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private List<CartItem> items;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+}
